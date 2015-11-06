bec52f62c8fd86ac347ea06b7699a8b3ee278446
Chunmun <xilasirc@gmail.com>
2015-11-06 21:31:00 +0800

    Working

--- rent.py
+++ rent.py
@@ -39,15 +39,17 @@ strategies = { 'A' : randomStrategy,
                'B' : randomStrategy,
                'C' : randomStrategy }
 
-strategies = { 'A' : cheapskateStrategy,
-               'B' : cheapskateStrategy,
-               'C' : cheapskateStrategy }
-
 strategies = { 'A' : makeRoomNStrategy(3),
                'B' : cheapskateStrategy,
                'C' : cheapskateStrategy }
 
+strategies = { 'A' : cheapskateStrategy,
+               'B' : cheapskateStrategy,
+               'C' : cheapskateStrategy }
+
 total_rent = 1000
+mesh_division = 100
+size = total_rent / mesh_division
 
 # ============================= Initialization =============================
 
@@ -56,15 +58,16 @@ class Point():
         if len(coords) != len(rooms):
             raise Exception('Wtf man, why mismatched dimensions? {} != {}'
                     .format(coords, rooms))
-        if abs(sum(coords) - total_rent) > 1:
+        if sum(coords) != mesh_division:
             raise Exception(
                     'Total coordinates does not add up to total rent! {} != {}'
-                    .format(coords, total_rent))
+                    .format(coords, mesh_division))
         if min(coords) < 0:
             raise Exception('Eh got negative values in the coordinates leh {}'
                     .format(coords))
         coords = tuple([0 if i < EPS else round(i,ROUNDING) for i in coords])
         self.coords = coords
+        self.label = housemates[(coords[0] + coords[2]) % len(housemates)]
 
         self.decisions = {}
         for h in housemates:
@@ -96,206 +99,38 @@ class Point():
     def __repr__(self):
         return str(self)
 
-class Triangle():
-    def __init__(self, pt1, pt2, pt3, initInner=False, name=""):
-        self.corners = [pt1, pt2, pt3]
-        self.all_points = []
-        self.mid_points = []
-        self.inner_triangles = []
-        self.corner_label = 'AAA'
-        self.good = False
-        self.name = name
+directions = { 'nw' : lambda pt: (pt[0]-1, pt[1]+1, pt[2]),
+               'n'  : lambda pt: (pt[0], pt[1]+1, pt[2]-1),
+               'e'  : lambda pt: (pt[0]+1, pt[1], pt[2]-1) }
 
-        self.initPointsFromCorners()
-        self.labels = ['ABC', 'BAC', 'CAB', 'CBA', 'BCA', 'ACB']
+def isGoodCoordinate(coord):
+    return min(coord) >= 0
 
-        if initInner:
-            self.initInnerTriangles()
+triangles = []
 
-    """
-    Arrangement of points
-          c0
-         /  \
-        /    \
-       m0  b  m2
-      /        \
-     /          \
-    c1 -- m1 -- c2
-    
-    """
-    def initPointsFromCorners(self):
-        c = self.corners
-        self.mid_points = []
-        for i in range(len(c)):
-            self.mid_points.append(c[i].findPoint(c[(i+1)%len(c)]))
-        self.barycentre_pt = c[0].findPoint(self.mid_points[1], 2/3)
-        self.all_points = self.mid_points + c + [self.barycentre_pt]
+for room1 in range(mesh_division+1):
+    for room2 in range(mesh_division+1):
+        room3 = mesh_division - room1 - room2
+        if room3 < 0:
+            continue
 
-    """
-    Arrangement of inner triangles
+        p = Point(room1, room2, room3)
 
-                  c0
-                  /\
-                 /| \
-                / |  \
-               /  |   \
-              /   |    \
-             /    |     \
-           m0   0 |  5  m2
-           /  .   |   .    \
-          /       b         \
-         /  1  .  |  .  4    \
-        /   .  2  |  3  .     \
-       / .        |         .  \
-      c1----------m1-----------c2
+        nw = directions['nw'](p)
+        n = directions['n'](p)
+        e = directions['e'](p)
 
-    Labelling of who to ask with Corner label AAA
-                   A
-                  /\
-                 /| \
-                / |  \
-               /  |   \
-              /   |    \
-             /    |     \
-            B   0 |  5   B
-           /  .   |   .    \
-          /       C         \
-         /  1  .  |  .  4    \
-        /   .  2  |  3  .     \
-       / .        |         .  \
-      A ----------B ----------- A
+        if isGoodCoordinate(n):
+            n = Point(*n)
+            if isGoodCoordinate(nw):
+                nw = Point(*nw)
+                triangles.append((p, nw, n))
+            if isGoodCoordinate(e):
+                e = Point(*e)
+                triangles.append((p, n, e))
 
-
-      After pulling
-
-      m0----------c0-----------m2
-       \ .        |         .  /
-        \   .  0  |  5  .     /
-         \  1  .  |  .  4    /
-          \       b         /
-           \  .   |   .    /
-           c1   2 |  3  c2
-             \    |     /
-              \   |    /
-               \  |   /
-                \ |  /
-                 \| /
-                  \/
-                  m1
-    """
-    def initChoicesFromCornerLabel(self):
-        self.choices = ""
-        for i in range(len(self.corner_label)):
-            l = self.corner_label[i]
-            self.choices += str(self.corners[i].decisions[l])
-        self.good = all([str(room) in self.choices for room in rooms])
-
-    def initInnerTriangles(self):
-        c = self.corners
-        m = self.mid_points
-        b = self.barycentre_pt
-        t0 = Triangle(c[0], m[0], b, False, self.name + '0')
-        t1 = Triangle(m[0], c[1], b, False, self.name + '1')
-        t2 = Triangle(b, c[1], m[1], False, self.name + '2')
-        t3 = Triangle(b, m[1], c[2], False, self.name + '3')
-        t4 = Triangle(m[2], b, c[2], False, self.name + '4')
-        t5 = Triangle(c[0], b, m[2], False, self.name + '5')
-
-        self.inner_triangles = [t0, t1, t2, t3, t4, t5]
-
-        # Set the inner labels and goodness
-        for i in range(len(self.inner_triangles)):
-            t = self.inner_triangles[i]
-            l = self.labels[i]
-            t.corner_label = l
-            t.initChoicesFromCornerLabel()
-            if t.good:
-                t.generateLabelsFromCornerLabel()
-
-
-    """
-    Labelling of who to ask from Corner Labels l0 l1 l2
-                  l0 
-                  /\
-                 /| \
-                / |  \
-               /  |   \
-              /   |    \
-             /    |     \
-           l3   0 |  5   l4
-           /  .   |   .    \
-          /      l5         \
-         /  1  .  |  .  4    \
-        /   .  2  |  3  .     \
-       / .        |         .  \
-     l1 ---------l6 ----------- l2
-    """
-    def generateLabelsFromCornerLabel(self):
-        if any([x not in self.corner_label for x in housemates]):
-            raise Exception('Cannot generate labels if non-distinct corner labels {}'
-                    .format(self.corner_label))
-
-        l0 = self.corner_label[0]
-        l1 = self.corner_label[1]
-        l2 = self.corner_label[2]
-        l3 = l2
-        l4 = l1
-        l6 = l0
-        # l5 is a function that gives that last label give 2 labels
-        l5 = lambda x, y: [z for z in housemates if z not in [x, y]][0]
-
-        self.labels = [l0+l3+l5(l0, l3),
-                       l3+l1+l5(l3, l1),
-                       l5(l1, l6)+l1+l6,
-                       l5(l6, l2)+l6+l2,
-                       l4+l5(l4, l2)+l2,
-                       l0+l5(l0, l4)+l4]
-
-
-    def getGoodInnerTriangleIndex(self):
-        t = self.inner_triangles
-        idx = [i for i in range(len(t)) if t[i].good][0]
-        return idx, t[idx]
-
-    def getGoodInnerTriangle(self):
-        return [i for i in self.inner_triangles if i.good][0]
-
-    def __str__(self):
-        return ", ".join(list(map(str, self.corners)))
-        #return "Triangle\n========\n" + "\n".join(list(map(str, self.corners))) + "\n======="
-
-    def __repr__(self):
-        return str(self)
-
-
-initial_triangle = Triangle(Point(0, 0, total_rent), Point(0, total_rent, 0), Point(total_rent, 0, 0), True)
-i = 1
-last_bary = initial_triangle.barycentre_pt
-
-print("Good Triangle at index {} with corners {}".format(*initial_triangle.getGoodInnerTriangleIndex()))
-
-def debug():
-    for i,t in enumerate(initial_triangle.inner_triangles):
-        print('inner triangle', i, t.corners, t.corner_label, t.choices)
-
-while True:
-    try:
-        initial_triangle = initial_triangle.getGoodInnerTriangle()
-        initial_triangle.initInnerTriangles()
-        new_bary = initial_triangle.barycentre_pt
-
-        debug()
-        print("Try {} into {} with choice {} => {}"
-                .format(i, initial_triangle.barycentre_pt,
-                    initial_triangle.corner_label, initial_triangle.choices))
-
-        print(initial_triangle)
-        print("Good Triangle", initial_triangle.getGoodInnerTriangleIndex())
-        i+=1
-        if new_bary == last_bary:
-            break
-        last_bary = new_bary
-    except Exception as e:
-        print(e.message)
-        break
-
+for t in triangles:
+    choices = [p.decisions[p.label] for p in t]
+    if len(set(choices)) == len(rooms):
+        print([[c * size for c in p.coords] for p in t])
+        
